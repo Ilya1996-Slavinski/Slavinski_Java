c5e0f975f3c15c2678c6e641ceb42931f1041035
Ilya-1996-Slavinski <slavinskij96@gmail.com>
2025-03-18 15:56:52 +0300

    NEW CODE

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,2 +1,19 @@
 public class Task2 {
+      public static void main(String[] args) {
+            checkSumSign(); }
+
+        private static void checkSumSign() {
+            int a = 7;
+            int b = 8;
+            if ((a + b) >= 0) {
+                System.out.println("The sum is positive");}
+
+            else if ((a + b) <0) {
+                System.out.println("The sum is negative");}
+
+            else {
+                System.out.println("    ");                }
+        }
+
+    }
 }
