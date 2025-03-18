ac0aef27965a4be219f41f2c7aed853df6d18ddc
Ilya-1996-Slavinski <slavinskij96@gmail.com>
2025-03-18 15:59:04 +0300

    NEW CODE

---
+++ src/main/java/Task3.java
@@ -0,0 +1,17 @@
+public class Task3 {
+    public static void main(String[] args) {
+        printColor();    }
+
+    private static void printColor() {
+        int c = 40;
+        System.out.println("Yellow");
+
+        if (c <= 0) {
+            System.out.println("Red");}
+
+        else if (c > 100){
+            System.out.println("Green");}
+
+        else if (c > 0 && c <= 100) {
+            System.out.println("Yellow");}
+    } }
