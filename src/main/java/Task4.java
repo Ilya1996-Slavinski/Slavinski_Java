d725a1b5949b9d8b0cc6675e6dbc814dc530988e
Ilya-1996-Slavinski <slavinskij96@gmail.com>
2025-03-18 16:00:18 +0300

    NEW CODE

---
+++ src/main/java/Task4.java
@@ -0,0 +1,18 @@
+public class task4 {
+   public static void main(String[] args) {
+        compareNumbers();}
+
+    public static void compareNumbers() {
+        int m = 6;
+        int n = 6;
+        if (m > n) {
+            System.out.println("m > n");
+        }
+        else if (m == n) {
+            System.out.println("m = n");
+        }
+        else {
+            System.out.println("m < n");
+        }
+    }
+}
